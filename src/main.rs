4f81a6a4e9268d78aeb88bbd5bb52286cd56e570
stopachka <stepan.p@gmail.com>
2019-05-12 14:47:35 -0700

    single-threaded

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,31 @@
+use std::io::prelude::*;
 use std::net::TcpListener;
+use std::net::TcpStream;
+
+fn handle_connection(mut stream: TcpStream) {
+  let mut buffer = [0; 512];
+  stream.read(&mut buffer).unwrap();
+  let contents = r#"
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <title>Hello!</title>
+    </head>
+    <body>
+      <h1>Hello!</h1>
+      <p>Hi from Rust</p>
+    </body>
+  </html>"#;
+  let response = format!("HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}", contents.len(), contents);
+  stream.write(response.as_bytes()).unwrap();
+  stream.flush().unwrap();
+}
 
 fn main() {
   let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
   for stream_res in listener.incoming() {
     let stream = stream_res.unwrap();
-    println!("Connection established!");
+    handle_connection(stream)
   }
 }
